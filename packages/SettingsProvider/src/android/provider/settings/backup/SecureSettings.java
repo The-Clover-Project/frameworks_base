732cb295eed814e8d26843cbb786ab58f06f81b1
ezio84 <brabus84@gmail.com>
2023-04-11 20:20:59 +0300

    base: Allow showing Ambient instead of fully waking [1/2]
    Hey Google, please kang only good features by iOS -_-'
    
    Ido Ben-Hur (YAAP):
    Separate for dt2w and lift to wake
    Better code
    
    [AOSPA: Kill Double tap to ambient display]
    
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Change-Id: Ib831888aadc601519a4a5f0b6576d44a337435b3
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -336,6 +336,8 @@ public class SecureSettings {
         Settings.Secure.IDENTITY_CHECK_PROMO_CARD_SHOWN,
         Settings.Secure.IDENTITY_CHECK_NOTIFICATION_VIEW_DETAILS_CLICKED,
         Settings.Secure.NOTIFICATION_BUNDLES_ALWAYS_EXPAND,
+        Settings.Secure.DOZE_TAP_GESTURE_AMBIENT,
+        Settings.Secure.DOZE_PICK_UP_GESTURE_AMBIENT,
         Settings.Secure.ACCESSIBILITY_TOP_ROW_KEY_TARGETS,
         Settings.Secure.ACCESSIBILITY_QUICK_ACCESS_TARGETS,
         Settings.Secure.NAVIGATIONBAR_KEY_ORDER,
