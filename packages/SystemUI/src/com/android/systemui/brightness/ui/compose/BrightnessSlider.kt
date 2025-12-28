734321f25bfd4c64fec09896143d72ab611f3b15
Michael Bestas <mkbestas@lineageos.org>
2025-12-28 17:20:29 +0200

    fixup! SystemUI: Add auto brightness toggle in brightness slider in new compose QS
    * Match disabled state tint with the QS tiles and the brightness slider
    * Move hardcoded content description to strings
    
    Change-Id: I5381a1e18c58e1e67abdbd8ab5a76a7029375a79

--- packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
+++ packages/SystemUI/src/com/android/systemui/brightness/ui/compose/BrightnessSlider.kt
@@ -391,7 +391,7 @@ private fun drawAutoBrightnessButton(
         targetValue = if (autoMode) {
             MaterialTheme.colorScheme.primary
         } else {
-            LocalAndroidColorScheme.current.surfaceEffect2
+            LocalAndroidColorScheme.current.surfaceEffect1
         }
     )
     val iconTint by animateColorAsState(
@@ -416,7 +416,7 @@ private fun drawAutoBrightnessButton(
     ) {
         Icon(
             painter = painterResource(painterRes),
-            contentDescription = "Auto brightness",
+            contentDescription = stringResource(R.string.accessibility_adaptive_brightness),
             tint = iconTint
         )
     }
